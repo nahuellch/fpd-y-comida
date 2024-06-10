a910f64d52e294e27de1ad57396c58fd2f456ff6
nahuellch <163903861+nahuellch@users.noreply.github.com>
2024-06-10 15:53:19 -0300

    Initial commit

---
+++ Assets/Prefabs/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+
+    public Text puntajeTxt;
+
+    int puntaje;
+    void  OnTriggerEnter(Collider col)
+    {
+        puntaje += 1;
+        puntajeTxt.text = puntaje.ToString();
+
+        if (col.gameObject.CompareTag("food"))
+        {
+           
+            Destroy(col.gameObject);
+        }
+    }
+}
